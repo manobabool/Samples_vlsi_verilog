2ca7e1a4f78c1fec9ab7ac2ea733534fd3a6e176
alsandeep <root@sandeep-sparky.(none)>
2017-11-09 20:31:21 +0530

    first commit

---
+++ UARTTESTING.v
@@ -0,0 +1,35 @@
+//////////////////////////////////////////////////////////////////////////////////
+// Company: CEM SOLUTIONS PVT LTD
+// Engineer: BASKARAN
+// 
+// Create Date:    18:42:44 11/08/2017 
+// Design Name: 
+// Module Name:    UART TESTING 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+
+
+
+module UART_TESTING(
+						 fpag_uart_rx 	,	
+        fpag_uart_tx
+						);
+
+	input          fpag_uart_rx;	
+	output         fpag_uart_tx;	
+	//wire	pipe1;
+	//assign	pipe1 = 	fpag_uart_rx;
+	assign	fpag_uart_tx = fpag_uart_rx	;
+
+endmodule
+
